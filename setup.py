8142b985da70b9b94166d8fff254f6581b041161
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2019-07-17 09:20:36 +0300

    simple item similarity based recommender system

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We are going to use the movielens to build a simple item similarity based recommender system.',
+    author='Hesbon',
+    license='MIT',
+)
